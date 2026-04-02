762ca919eeb5df0cdcd137fb219e84ceac6fdc9b
cheskag <114804984+cheskag@users.noreply.github.com>
2026-04-02 18:33:07 +0800

    Delete analyze_disagreements.py

--- analyze_disagreements.py
+++
@@ -1,4 +0,0 @@
-MONGO_URI=mongodb+srv://jadccryptogauge:123456789%21%40%23%24%25%5E%26%2A%28@cluster0.9gr5xhk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-MONGO_TRAINING_URI=mongodb+srv://jadccryptogauge:123456789%21%40%23%24%25%5E%26%2A%28@cluster0.9gr5xhk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-MONGO_DB=dataset4JADC
-MONGO_COLLECTION=cryptogauge
